8b941f87a2a980e623362548b5b09f9c4936386c
BIT <BIT@DESKTOP-L3CKIRK>
2018-03-10 20:52:30 +0900

    again

--- src/com/cafe24/bookmall/app/Execute.java
+++ src/com/cafe24/bookmall/app/Execute.java
@@ -9,21 +9,27 @@ import com.cafe24.bookmall.dao.test.OrderListDAOTest;
 
 public class Execute {
 	public static void main(String[] args) {
+		System.out.println("==========카테고리 리스트==========");
 		CategoryDAOTest.insertTest();
 		CategoryDAOTest.getListTest();
 		
+		System.out.println("==========상품 리스트==========");
 		BookDAOTest.insertTest();
 		BookDAOTest.getListTest();
 		
+		System.out.println("==========회원 리스트==========");
 		MemberDAOTest.insertTest();
 		MemberDAOTest.getListTest();
 		
+		System.out.println("==========주문 리스트==========");
 		OrderDAOTest.insertTest();
 		OrderDAOTest.getListTest();
 		
+		System.out.println("==========카트 리스트==========");
 		CartDAOTest.insertTest();
 		CartDAOTest.getListTest();
 		
+		System.out.println("==========주문 도서 리스트==========");
 		OrderListDAOTest.insertTest();
 		OrderListDAOTest.getListTest();
 		
